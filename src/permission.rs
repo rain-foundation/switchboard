1048cf24c18666676e84146ee95e32f49ae39959
CanardMandarin <thibault.marboud@gmail.com>
2024-11-14 04:23:32 +0000

    test

--- src/permission.rs
+++ src/permission.rs
@@ -56,7 +56,7 @@ pub struct PermissionSetParams {
 }
 
 impl Discriminator for PermissionSet<'_> {
-    const DISCRIMINATOR: [u8; 8] = [211, 122, 185, 120, 129, 182, 55, 103];
+    const DISCRIMINATOR: &'static [u8] = &[211, 122, 185, 120, 129, 182, 55, 103];
 }
 
 impl<'info> PermissionSet<'info> {
@@ -67,7 +67,7 @@ impl<'info> PermissionSet<'info> {
     ) -> anchor_lang::Result<Instruction> {
         let accounts = self.to_account_metas(None);
 
-        let mut data: Vec<u8> = PermissionSet::discriminator().try_to_vec()?;
+        let mut data: Vec<u8> = PermissionSet::DISCRIMINATOR.try_to_vec()?;
         let mut param_vec: Vec<u8> = params.try_to_vec()?;
         data.append(&mut param_vec);
 
